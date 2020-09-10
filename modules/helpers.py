423d11eb18bf96d5264c3b346f9cc43369b65b44
Luka Robajac <luka.robajac@gmail.com>
2020-09-11 01:43:04 +0200

    Table headers/values are now mapped to a single variable so it doesn't have to be changed in multiple spots. Exception caught when no results found for a query.

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,10 +1,11 @@
 import requests
 
-def generate_headers(args):
-    headers = ["Company name", "Phone number", "Address"]
-    if args.scrape_website:
-        headers.append("Website")
-    return headers
+def generate_headers(args, example_dict):
+    headers = example_dict.keys()
+    if not args.scrape_website:
+        headers.remove("website")
+    
+    return [header.capitalize() for header in headers]
 
 def print_table_headers(worksheet, headers):
     col = 0
@@ -14,8 +15,8 @@ def print_table_headers(worksheet, headers):
 
 def write_data_row(worksheet, data, row):
     col = 0
-    for val in data:
-        worksheet.write(row, col, val)
+    for key in data:
+        worksheet.write(row, col, data[key])
         col += 1
 
 def get_website_url(url):
--- modules/scraper.py
+++ modules/scraper.py
@@ -27,21 +27,29 @@ def scrape(args):
     # Initialize workbook / worksheet
     workbook = xlsxwriter.Workbook('ScrapedData_GoogleMaps.xlsx')
     worksheet = workbook.add_worksheet()
-    headers = generate_headers(args)
+
+    # Headers and data
+    data = {
+        "name": "",
+        "phone": "",
+        "address": "",
+        "website": ""
+    }
+    headers = generate_headers(args, data)
     print_table_headers(worksheet, headers)
 
     # Start from second row in xlsx, as first one is reserved for headers
     row = 1
 
     # Remember scraped addresses to skip duplicates
-    addresses_scraped = []
+    addresses_scraped = {}
 
     start_time = time.time()
 
     for place in SETTINGS["PLACES"]:
         # Go to the index page
         driver.get(SETTINGS["MAPS_INDEX"])
-        
+
         # Build the query string
         query = "{0} {1}".format(SETTINGS["BASE_QUERY"], place)
         print("Moving on to {0}".format(place))
@@ -50,10 +58,13 @@ def scrape(args):
         q_input = driver.find_element_by_name("q")
         q_input.send_keys(query, Keys.ENTER)
         
-        # Wait for the results page to load
-        _ = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, BOX_CLASS))
-        )
+        # Wait for the results page to load. If no results load in 10 seconds, continue to next place
+        try:
+            w = wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, BOX_CLASS))
+            )
+        except:
+            continue
 
         # Loop through pages and results
         for _ in range(0, SETTINGS["PAGE_DEPTH"]):
@@ -62,29 +73,36 @@ def scrape(args):
 
             # Loop through all boxes and get the info from it and store into an excel
             for box in boxes:
+                # Just get the values, add only after we determine this is not a duplicate (or duplicates should not be skiped)
                 name = box.find_element_by_class_name("section-result-title").find_element_by_xpath(".//span[1]").text
 
                 address = box.find_element_by_class_name("section-result-location").text
 
-                if address in addresses_scraped and not args.skip_duplicate_addresses:
+                scraped = address in addresses_scraped
+
+                if scraped and args.skip_duplicate_addresses:
                     print("Skipping {0} as duplicate by address".format(name))
                 else:
-                    data = []
-                    data.append(name)
-                    addresses_scraped.append(address)
-        
-                    # Just to have something to follow the progress
-                    print("Currently scraping on: {0}".format(name))
+                    # Initiate the list and add to it only now
+                    data["name"] = name
+                    data["address"] = address
 
                     phone = box.find_element_by_class_name("section-result-phone-number").find_element_by_xpath(".//span[1]").text
-                    data.append(phone)
 
-                    data.append(address)
-                    
+                    data["phone"] = phone
+
+                    if scraped:
+                        addresses_scraped[address] += 1
+                        print("Currently scraping on: {0}, for the {1}. time".format(name, addresses_scraped[address]))
+                    else:
+                        addresses_scraped[address] = 1
+                        print("Currently scraping on: {0}".format(name))
+                         
+                    # Only if user wants to get the URL to, get it
                     if args.scrape_website:
                         url = box.find_element_by_class_name("section-result-action-icon-container").find_element_by_xpath("./..").get_attribute("href")
                         website = get_website_url(url)
-                        data.append(website)
+                        data["website"] = website
                     
                     write_data_row(worksheet, data, row)
                     row += 1
@@ -94,7 +112,7 @@ def scrape(args):
             try:
                 next_page_link.click()
             except WebDriverException:
-                print("No more pages, navigation not clickable")
+                print("No more pages for this search. Advancing to next one.")
                 break
 
             # Wait for the next page to load
